3884e9ccb8258ea40cd5918a6901051745c6d9d3
Nicolas Coulaud <coulauddev@gmail.com>
2018-10-25 23:31:23 +0200

    omg

--- script.js
+++ script.js
@@ -1,34 +1,73 @@
+
+
+
+
 $(document).ready(function () {
+    $('.divLettre').hide();
+    $('#randomBtn').hide();
+    shuffleElements( $('.lettre'));
+})
 
-    input = $('#input');
-    btn = $('#submit');
-    ol = $('#ol');
-    compteur = 1;
+$('#startBtn').on('click', function () {
+    $(this).hide();
+    $('.divLettre').show();
+    $('#randomBtn').show();
+})
 
-    btn.click(function () {
-        ol.append($('<li id="'+compteur+'"></li>').append(input.val()+ ' <a class="up">UP</a> <a class="down">DOWN</a> <a class="rem">REMOVE</a>'))
-        input.val('');
-        compteur += 1;
-    });
-
-
-    $(document).on('click', '.up', function () {
-        laura = $(this).parent();
-        $(laura).insertBefore($(laura).prev());
-        console.log(document.getElementById('ol').firstChild.getElementbyId());
-    });
-
-    $(document).on('click', '.down', function () {
-        laura = $(this).parent();
-        $(laura).insertAfter($(laura).next());
-    });
-
-    $(document).on('click', '.rem', function () {
-        nico = $(this).parent();
-        $(nico).remove();
-    })
-
+$(document).on('click', '.lettre', function () {
+    $(this).siblings().removeClass("sel");
+    $(this).addClass("sel");
 
 })
 
+$(document).keydown(function(e) {
+    if(e.which == 37) {
+        $('.sel').insertBefore($('.sel').prev());
+    }else if (e.which == 39)
+    {
+        $('.sel').insertAfter($('.sel').next());
 
+    }
+    if($('.lettre').eq(0).hasClass("L") && $('.lettre').eq(2).hasClass("U") && $('.lettre').eq(3).hasClass("R")){
+        alert('You did it ! omg !');
+    }
+});
+
+
+$('#randomBtn').click(function() {
+    shuffleElements( $('.lettre') );
+});
+
+function shuffleElements($elements) {
+    var i, index1, index2, temp_val;
+
+    var count = $elements.length;
+    var $parent = $elements.parent();
+    var shuffled_array = [];
+
+
+    // populate array of indexes
+    for (i = 0; i < count; i++) {
+        shuffled_array.push(i);
+    }
+
+    // shuffle indexes
+    for (i = 0; i < count; i++) {
+        index1 = (Math.random() * count) | 0;
+        index2 = (Math.random() * count) | 0;
+
+        temp_val = shuffled_array[index1];
+        shuffled_array[index1] = shuffled_array[index2];
+        shuffled_array[index2] = temp_val;
+    }
+
+    // apply random order to elements
+    $elements.detach();
+    for (i = 0; i < count; i++) {
+        $parent.append( $elements.eq(shuffled_array[i]) );
+    }
+}
+
+if($('.lettre').eq(0).hasClass("A")){
+    alert('omg');
+}
